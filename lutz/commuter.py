e270b281ef26d55a9fbe0e99fa72acde0ba05c5a
w9i <w9i@w9is-MacBook.local>
2023-06-23 00:57:53 +0300

    finish the chapter 30

--- lutz/commuter.py
+++ lutz/commuter.py
@@ -7,11 +7,47 @@ class Commuter1:
     def __radd__(self, other):
         print('radd', self.val, other)
         return other + self.val
+# Для подлинно коммутативных операций, которые не тербуют учета позиций
+# иногда вполне достаточно использовать __add__ для __radd__
+class Commuter2:
+    def __init__(self, val):
+        self.val = val
+    def __add__(self, other):
+        print('add', self.val, other)
+        return self.val + other
+    def __radd__(self, other):
+        return self.__add__(other)  # Явно вызвать __add__
+    
+class Commuter3:
+    def __init__(self, val):
+        self.val = val
+    def __add__(self, other):
+        print('add', self.val, other)
+        return self.val + other
+    def __radd__(self, other):
+        return self + other
+    
+class Commuter4:
+    def __init__(self, val):
+        self.val = val
+    def __add__(self, other):
+        print('add', self.val, other)
+        return self.val + other
+    __radd__ = __add__
 
+class Commuter5:
+    def __init__(self, val):
+        self.val = val
+    def __add__(self, other):
+        if isinstance(other, Commuter5):
+            other = other.val
+        return Commuter5(self.val + other)
+    def __radd__(self, other):
+        return Commuter5(other + self.val)
+    def __str__(self):
+        return '<Commuter5: %s>' % self.val
 if __name__ == '__main__':
-    x = Commuter1(88)
-    y = Commuter1(99)
+    x = Commuter5(1)
+    y = Commuter5(2)
 
-    x + 1  # __add__: Экземпляр + не экземпляр
-    1 + y  # __radd__: Не экземпляр + экземпляр
-    x + y  # __add__: Экземпляр + экземпляр, запускает __radd__
+    print(x + y)
