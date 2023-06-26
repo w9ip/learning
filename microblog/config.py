3df70af91eb183eda272a9ee8c3be6ab2a3924ee
w9i <w9i@w9is-MacBook.local>
2023-06-26 20:18:58 +0300

    finish chapter 4 starting chapter 5

--- microblog/config.py
+++ microblog/config.py
@@ -1,6 +1,9 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'yoo\'ll-never-guess'
-    print(SECRET_KEY)
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
--- microblog/microblog.py
+++ microblog/microblog.py
@@ -1,1 +1,6 @@
-from app_pkg import app
+from app_pkg import app, db
+from app_pkg.models import User, Post
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
