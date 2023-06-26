5366c51f646e4f32a4b0071bdd0e2c70c520d142
w9i <w9i@w9is-MacBook.local>
2023-06-26 15:26:08 +0300

    chapter2: separate html & python code

--- microblog/app_pkg/routes.py
+++ microblog/app_pkg/routes.py
@@ -1,6 +1,9 @@
+from flask import render_template
 from . import app
 
 
 @app.route('/')
 @app.route('/index')
-def index(): return 'Hello, World!'
+def index():
+    user = {'username': 'Miguel'}
+    return render_template('index.html', title='Home', user=user)
