6cde8f1da714edb236bddc5548b76888b743b428
w9i <w9i@w9is-MacBook.local>
2023-06-27 14:55:54 +0300

    finish chapter 7 start chapter 8

--- microblog/microblog.py
+++ microblog/microblog.py
@@ -1,6 +1,13 @@
 from app_pkg import app, db
 from app_pkg.models import User, Post
+import logging
+from logging.handlers import SMTPHandler
+
 
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'User': User, 'Post': Post}
+
+
+if __name__ == '__main__':
+    app.run(debug=False)
