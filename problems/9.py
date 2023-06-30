ead97b182f641d8a6b8cb953ac76694e44a80d84
w9i <w9i@w9is-MacBook.local>
2023-06-30 14:11:50 +0300

    4.6 Матрицы. Часть 3/9

--- problems/9.py
+++ problems/9.py
@@ -1,6 +1,17 @@
-def main():
-    pass
+# N-rows
+# M-columns
 
+N, M = map(int, input().split())
+gen = (e for e in range(1, N * M + 1))
+matrix = []
 
-if __name__ == "__main__":
-    pass
+for row in range(1, N + 1):
+    matrix.append([])
+    for row_elem in range(1, M + 1):
+        matrix[row - 1].append(str(next(gen)).ljust(2, ' '))
+
+for i in range(N):
+    if i % 2 == 0:
+        print(*matrix[i])
+    else:
+        print(*matrix[i][::-1])
