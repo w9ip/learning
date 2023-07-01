d45776174ad99a036dadab6f6cf19e0aed6e15f9
w9i <w9i@w9is-MacBook.local>
2023-07-01 03:31:01 +0300

    insertion sort algorithm

---
+++ algorithms/insertion_sort.py
@@ -0,0 +1,10 @@
+def insertion_sort(array):
+    for i in range(1, len(array)):
+        j=i
+        while (j > 0) and (array[j] < array[j-1]):
+            array[j], array[j-1] = array[j-1], array[j]
+            j=j-1
+    return array
+
+print(insertion_sort(list('INSERTIONSORT')))
+
---
+++ algorithms/insertion_sort_my_version.py
@@ -0,0 +1,11 @@
+def insertion_sort(alist):
+    for i in range(1, len(alist)):
+        temp = alist[i]
+        j = i - 1
+        while (j >= 0 and temp < alist[j]):
+            alist[j + 1] = alist[j]
+            j = j - 1
+        alist[j + 1] = temp
+ 
+ 
+insertion_sort(list('insertionsort'))
