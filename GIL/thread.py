492f9e7d6a9f31bfcb07f611b45a310ca193ac43
w9i <w9i@w9is-MacBook.local>
2023-08-03 18:11:48 +0300

    learning

--- GIL/thread.py
+++ GIL/thread.py
@@ -1,6 +1,24 @@
-def main():
-    pass
+import threading
+
+x = 2
 
 
-if __name__ == "__main__":
-    pass
+def f(n):
+    global x
+    x += n
+    print(x)
+
+
+def j(n):
+    global x
+    x *= n
+    print(x)
+
+
+if __name__ == '__main__':
+    t1 = threading.Thread(target=f, args=(2,), daemon=True)
+    t2 = threading.Thread(target=j, args=(2,), daemon=True)
+    t1.start()
+    t2.start()
+    t1.join()
+    t2.join()
---
+++ problems/codewars.py
@@ -0,0 +1,4 @@
+def make_negative(n):
+    return -n if n >= 0 else n
+
+print(make_negative(0))
