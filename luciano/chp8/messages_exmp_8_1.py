9a34e865f298a12666f1aa8abeb350ebb233157f
w9i <w9i@w9is-MacBook.local>
2023-08-19 02:02:04 +0300

    chp8

---
+++ luciano/chp8/messages_exmp_8_1.py
@@ -0,0 +1,6 @@
+# Функция show_count без аннотации типов.
+def show_count(count: int, word: str) -> str:
+    if count == 1:
+        return f'1 {word}'
+    count_str = str(count) if count else 'no'
+    return f'{count_str} {word}s'
