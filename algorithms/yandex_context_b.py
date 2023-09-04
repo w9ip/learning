1963593cace62cf4a95a846562bf97c3c09f1160
w9i <m.p1nk@yandex.com>
2023-09-04 22:16:54 +0300

    9 tests out of 13 successfully

--- algorithms/yandex_context_b.py
+++ algorithms/yandex_context_b.py
@@ -9,38 +9,36 @@ rockets_logs = [
 sorted_logs = {}
 
 for day, hour, minute, id, status in rockets_logs:
-    lst = (day, hour, minute, status)
+    lst = (int(day)*24*60+int(hour)*60+int(minute), status)
     sorted_logs.setdefault(id, [])
     insort(sorted_logs[id], lst)
 
-
-def calculate_interval(date1, date2):
-    day1, hour1, minute1 = date1
-    day2, hour2, minute2 = date2
-
-
+res_dir = {}
 for rocket, logs in sorted_logs.items():
     A, B, C, S = 0, 0, 0, 0
     flag = 0
     rocket_sum_minute = 0
-    for day, hour, minute, status in logs:
-        print(day, hour, minute, status)
+    for minutes, status in logs:
         if status == 'A':
-            A = day, hour, minute, status
+            A = minutes
         if status == 'B':
-            B = day, hour, minute, status
+            B = minutes
         if status == 'C':
-            C = day, hour, minute, status
+            C = minutes
             flag = 1
         if status == 'S':
-            S = day, hour, minute, status
+            S = minutes
             flag = 1
         if flag:
             if A and B and C:
-                print(f'A={A}, B={B}, C={C}')
+                rocket_sum_minute += (C-B) + (B-A)
             elif A and C:
-                print(f'A={A}, C={C}')
+                rocket_sum_minute += (C-A)
             elif A and B and S:
-                print(f'A={A}, B={B}, S={S}')
+                rocket_sum_minute += (S - B) + (B - A)
             flag = 0
             A, B, C, S = 0, 0, 0, 0
+    res_dir.setdefault(rocket, rocket_sum_minute)
+
+for i in sorted(res_dir.keys())[::-1]:
+    print(res_dir[i], end=' ')
