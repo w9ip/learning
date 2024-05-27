bb06afd8a5a86c35ed70102980b36b121e734357
w9ip <m.p1nk@yandex.ru>
2024-05-27 17:07:36 +0300

    training

--- tests.py
+++ tests.py
@@ -1,16 +1,6 @@
-def run_test_func(inp_q, out_q):
-    lst = []
+from itertools import permutations
 
-    with open('tests.txt', 'r', encoding='UTF8') as tests:
-        for string in tests:
-            print(repr(string))
-            lst += string.split()
-    print(lst)
-    for i in range(0, len(lst), inp_q + out_q + 1):
-        print(
-            f'Номер теста: {lst[i]}, Входные данные: {lst[i + 1:i + inp_q + 1]}, Выходные данные: {lst[i + inp_q + 1:i + inp_q + out_q + 1]}'
-        )
-
-
-if __name__ == '__main__':
-    run_test_func(1, 1)
+s = 'ТОДЕЛ'
+perm_set = permutations(s)
+for val in perm_set:
+    print(''.join(val))
