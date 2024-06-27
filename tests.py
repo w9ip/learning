af2feb514d5f5b291a900de798086b4153ecdbc0
w9ip <m.p1nk@yandex.ru>
2024-06-27 19:09:24 +0300

    training

--- tests.py
+++ tests.py
@@ -1,6 +1,9 @@
-from itertools import permutations
-
-s = 'ТОДЕЛ'
-perm_set = permutations(s)
-for val in perm_set:
-    print(''.join(val))
+d = {}
+c = 0
+with open('tests.txt', 'r', encoding='UTF8') as tests:
+    title = next(tests)
+    print(title)
+    for s in tests:
+        print(s)
+        print(repr(s))
+print(d)
